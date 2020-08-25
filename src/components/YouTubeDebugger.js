4ba4c623104dd94dc6376dedad20817c2f51dd90
github username <lushiyun@outlook.com>
2020-08-24 20:36:28 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,57 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+import { toughCookie } from 'jsdom'
+
+export class YouTubeDebugger extends Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrateClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolutionClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button 
+          className="bitrate"
+          onClick={this.handleBitrateClick}>
+        </button>
+        <button
+          className="resolution"
+          onClick={this.handleResolutionClick}>
+        </button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger
+
